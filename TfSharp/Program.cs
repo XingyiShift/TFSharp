00266bd0fc4076fbe7c3cff9fceb697d54b06bd2
Xingyi WANG <xingyi.wang@shift-technology.com>
2019-06-05 11:45:22 +0200

    Add project files.

---
+++ TfSharp/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace TfSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
